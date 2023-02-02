706b860112d578de4215fd113f5c218d2f25f190
Wendel Duarte <105465397+Wendeldev87@users.noreply.github.com>
2023-02-02 13:27:21 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Olá Mundo! ");
+		System.out.println(" Seja muito bem vindo Wendel ao incrível mundo do Java, muito sucesso! ");
+
+	}
+
+}
